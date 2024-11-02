b210c3bb5099f9895be3097bc6d061ac9a91f091
ishanchamika <ishanchami9@gmail.com>
2024-11-02 18:17:42 +0530

    create student request page

--- src/Components/Request.js
+++ src/Components/Request.js
@@ -1,7 +1,10 @@
 import React , { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
 import { extendTheme, styled } from '@mui/material/styles';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -12,44 +15,14 @@ import { AppProvider } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import { DataGrid } from '@mui/x-data-grid';
-import Paper from '@mui/material/Paper'
+import Paper from '@mui/material/Paper';
+import DialogBox from './DialogBox';
+import { colors } from '@mui/material';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-const paginationModel = { page: 0, pageSize: 5 };
 
 export default function DataTable() 
 {
+    // _________fetch data handle__________________________________
     const [requestsData, setRequestData] = useState([]);
 
     const handleFetchData = async () =>
@@ -73,17 +46,83 @@ export default function DataTable()
         handleFetchData();
     }, []);
 
+
+    //__________handle dialog Box___________________________
+    const paginationModel = { page: 0, pageSize: 5 };
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'name', headerName: 'Name', width: 170 },
+        { field: 'email', headerName: 'Email', width: 170 },
+        { field: 'nic', headerName: 'NIC', type: 'number', width: 150},
+        { field: 'date', headerName: 'Date', description: 'This column has a value getter and is not sortable.', sortable: false, width: 150, valueGetter: (value, row) => `${row.date || ''}`},
+        { field: 'option', headerName: 'Option', width: 300,
+          renderCell: (params) => (
+            <>
+            <Stack direction="row" spacing={1}>
+              <Button onClick={() => handleReject(params.row.id)} variant="outlined" startIcon={<DeleteIcon />}>Reject</Button>
+              <Button onClick={() => handleAccept(params.row.id)} variant="contained" endIcon={<SendIcon />}>Accept</Button>
+            </Stack>
+            </>
+          ),
+        },
+        { field: 'status', headerName: 'Current Status', width: 170 },
+      ];
+
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogContent, setDialogContent] = useState("");
+  const [dialogAction, setDialogAction] = useState(null);
+
+  const handleAccept = (id) => {
+    setDialogContent("Are you sure you want to accept this request?");
+    setDialogAction(() => () => confirmAction("Accepted", id));
+    setDialogOpen(true);
+  };
+
+  const handleReject = (id) => {
+    setDialogContent("Are you sure you want to reject this request?");
+    setDialogAction(() => () => confirmAction("Rejected", id));
+    setDialogOpen(true);
+  };
+
+  const confirmAction = async (action, id) => 
+  {
+    try{
+        const response = await axios.put('http://localhost:5005/api/requests-update', { id , action });
+        console.log(`Request ${action}ed successfully:`, response.data);
+        handleFetchData();
+    }
+    catch(err)
+    {
+        console.error(`Error ${action}ing request:`, err);
+    }
+    finally
+    {
+        setDialogOpen(false);
+    }
+  };
+
+
+
   return (
+    <>
+    <DialogBox
+        open={dialogOpen}
+        onClose={() => setDialogOpen(false)}
+        onConfirm={dialogAction}
+        title="Confirmation"
+        content={dialogContent}
+      />
     <Paper sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={requestsData.response}
         columns={columns}
         initialState={{ pagination: { paginationModel } }}
         pageSizeOptions={[5, 10]}
-        checkboxSelection
+        // checkboxSelection
         sx={{ border: 0 }}
       />
-      <p>HELLLOOOOOOO</p>
     </Paper>
+    </>
   );
 }
